aa15819b904f668ad01796ff93c310298895aaa7
Rian Finnegan <flanagan89@gmail.com>
2017-07-26 14:27:01 +1000

    removed bad init command

--- src/dialogs/dialogIndex.ts
+++ src/dialogs/dialogIndex.ts
@@ -6,4 +6,4 @@ import someBasicDialog from './samples/someBasicDialog';
 import luisDialog from './samples/luisDIalog';
 import qnaDialog from './samples/qnaDialog';
 
-export {dataDialog, someBasicDialog, luisDialog, qnaDialog}; 
+export { someBasicDialog, luisDialog, qnaDialog}; 
